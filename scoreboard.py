5abc84c735388b4694d2368ffbc30a6ce2c19589
freddy <freedomochulor22gmail.com>
2024-09-06 17:47:16 +0100

    detecting wall collisions

--- scoreboard.py
+++ scoreboard.py
@@ -15,6 +15,11 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font= FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg= "GAME OVER", align=ALIGNMENT, font=FONT)
+
+
     def add_score(self):
         self.score += 1
         self.clear()
